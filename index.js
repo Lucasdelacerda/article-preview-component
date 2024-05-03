2d18d4f37c2a83bcf18dccf4b7f77d2f5cc48a74
Lucas de Lacerda <106289445+Lucasdelacerda@users.noreply.github.com>
2024-05-03 16:40:11 -0300

    final commit:

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 
 const iconContainer = document.querySelector(".share-icon-container")
 const iconShare = document.querySelector(".share-icon path")
+const shareContainer = document.querySelector(".share-container")
 
 iconContainer.addEventListener('mouseenter', () => {
 	iconShare.setAttribute('fill', 'white')
-	iconContainer.style.background = 'hsl(217, 19%, 35%)'
+	iconContainer.style.background = 'hsl(214, 17%, 51%)'
+	shareContainer.style.display = 'flex'
 });
 
+shareContainer.addEventListener('mouseenter', () => {
+	iconShare.setAttribute('fill', 'white')
+	iconContainer.style.background = 'hsl(214, 17%, 51%)'
+	shareContainer.style.display = 'flex'
+});
 
+shareContainer.addEventListener('mouseleave', () => {
+	iconShare.setAttribute('fill', '#6E8098')
+	iconContainer.style.background = '#EDF1FA'
+	shareContainer.style.display = 'none'
+});
+ 
+ iconContainer.addEventListener('mouseleave', () => {
+	iconShare.setAttribute('fill', '#6E8098')
+	iconContainer.style.background = '#EDF1FA'
+	shareContainer.style.display = 'none'
+});
